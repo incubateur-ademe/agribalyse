753168a0ee59ff12eb65a3df0fcbc8b5774f4f85
Florian Panchout <florian@Florians-MacBook-Pro.local>
2020-09-28 16:25:07 +0200

    Fix svg consommation

--- src/views/comparator/aliment/informations/stepsCard/step/Consommation.js
+++ src/views/comparator/aliment/informations/stepsCard/step/Consommation.js
@@ -24,13 +24,14 @@ const Path = styled.path`
 export default function Transformation(props) {
   return (
     <Svg viewBox='0 0 196.53 174.16'>
-      <Line  x1='131.2' y1='82.83' x2='190.28' y2='82.83' />
+      <Line x1='131.2' y1='82.83' x2='190.28' y2='82.83' />
       <Path
-        
+        white={props.white}
         d='M203.8,166.19H203M180.9,251.27H101a22.9,22.9,0,0,1-22.9-22.9V166.19H203.8v62.18A22.9,22.9,0,0,1,180.9,251.27Z'
         transform='translate(-71.82 -83.36)'
       />
       <Path
+        white={props.white}
         d='M155.6,143.51a19.41,19.41,0,0,1-.49-27,19.39,19.39,0,0,0-.48-26.94m-27.39,53.9a19.41,19.41,0,0,1-.49-27,19.39,19.39,0,0,0-.48-26.94'
         transform='translate(-71.82 -83.36)'
       />
