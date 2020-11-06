cbf97b6481c0fa7134d2c2bc7f7ecfa666b65ad8
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-11-06 10:39:10 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { QueryParamProvider } from 'use-query-params'
 
 import { GlobalStyle } from 'utils/styles'
-
 import SearchProvider from 'components/providers/SearchProvider'
 
 import OutdatedBrowser from 'components/OutdatedBrowser'
@@ -11,39 +11,6 @@ import Home from 'views/Home'
 import Comparator from 'views/Comparator'
 
 function App() {
-  const [aliments, setAliments] = useState([])
-
-  const [allCategories, setAllCategories] = useState([])
-  useEffect(() => {
-    let tempCategories = []
-
-    for (let aliment of aliments) {
-      if (!tempCategories.find(category => category.title === aliment.groupe)) {
-        tempCategories.push({
-          title: aliment.groupe,
-          subCategories: []
-        })
-      }
-
-      if (
-        !tempCategories
-          .find(category => category.title === aliment.groupe)
-          .subCategories.find(category => category === aliment.sous_groupe)
-      ) {
-        tempCategories
-          .find(category => category.title === aliment.groupe)
-          .subCategories.push(aliment.sous_groupe)
-      }
-    }
-    setAllCategories(tempCategories)
-  }, [aliments])
-
-  useEffect(() => {
-    fetch('/data/Agribalyse.json')
-      .then(res => res.json())
-      .then(data => setAliments(data))
-  }, [])
-
   const outdatedBrowser = uaString => {
     uaString = uaString || window.navigator.userAgent
     var match = /\b(MSIE |Trident.*?rv:|Edge\/)(\d+)/.exec(uaString)
@@ -52,24 +19,26 @@ function App() {
 
   return (
     <Router>
-      <GlobalStyle />
-      {outdatedBrowser() ? (
-        <OutdatedBrowser />
-      ) : (
-        <>
-          <SearchProvider>
-            <Banner aliments={aliments} />
-            <Switch>
-              <Route path='/aliments/:ciqual_code?/:nom_francais'>
-                <Comparator aliments={aliments} categories={allCategories} />
-              </Route>
-              <Route path={['/']}>
-                <Home />
-              </Route>
-            </Switch>
-          </SearchProvider>
-        </>
-      )}
+      <QueryParamProvider ReactRouterRoute={Route}>
+        <GlobalStyle />
+        {outdatedBrowser() ? (
+          <OutdatedBrowser />
+        ) : (
+          <>
+            <SearchProvider>
+              <Banner />
+              <Switch>
+                <Route path='/aliments/:code_agb?'>
+                  <Comparator />
+                </Route>
+                <Route path={['/']}>
+                  <Home />
+                </Route>
+              </Switch>
+            </SearchProvider>
+          </>
+        )}
+      </QueryParamProvider>
     </Router>
   )
 }
