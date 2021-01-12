a65297fcc056ee182a898b822413eeb59f289486
florianpanchout <hi@florianpanchout.com>
2021-01-12 11:29:17 +0100

    test 301 ter

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'build')))
+//app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', function (req, res) {
   if (req.get('host').includes('app.agribalyse.fr')) {
@@ -24,7 +24,7 @@ app.get('/*', function (req, res) {
     )
     res.status(301).send()
   }
-  res.send('root')
+  //res.send('root')
   // res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.listen(port)
