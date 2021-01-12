8f3c8feb5b9664cea60a32a3bbb15f2721fada4f
florianpanchout <hi@florianpanchout.com>
2021-01-12 10:39:00 +0100

    Redirection 301

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express = require('express');
-// const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8080
+const app = express()
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'build')))
+
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+  if (req.get('host').includes('app.agribalyse.fr')) {
+    res.set('location', 'https://agribalyse.fr/app/' + req.originalUrl)
+    res.status(301).send()
+  }
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+app.listen(port)
