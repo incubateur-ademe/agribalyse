ae7eb16d3c01f947e4ac54bf012cb82c675e062b
florianpanchout <hi@florianpanchout.com>
2021-01-12 10:57:41 +0100

    Revert Test

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('*', function (req, res) {
-  //if (req.get('host').includes('app.agribalyse.fr')) {
-  res.set('location', 'https://agribalyse.fr/app' + req.originalUrl)
-  res.status(301).send()
-  //}
+app.get('/*', function (req, res) {
+  if (req.get('host').includes('app.agribalyse.fr')) {
+    res.set('location', 'https://agribalyse.fr/app' + req.originalUrl)
+    res.status(301).send()
+  }
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.listen(port)
