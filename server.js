d1dd7b0f7a463b096788a810d4917f85105664be
florianpanchout <hi@florianpanchout.com>
2021-01-12 11:45:55 +0100

    301 test

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8080
+
 const app = express()
-app.use(express.static(__dirname))
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
@@ -15,4 +16,5 @@ app.get('/*', function (req, res) {
   }
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
 app.listen(port)
