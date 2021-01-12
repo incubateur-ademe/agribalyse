a6e513dbd551147d8ab85df1c47c0a78058a525f
florianpanchout <hi@florianpanchout.com>
2021-01-12 11:23:49 +0100

    Test 301

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
-app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'build')))
+//app.use(express.static(__dirname))
+//app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', function (req, res) {
   if (req.get('host').includes('app.agribalyse.fr')) {
@@ -13,16 +13,18 @@ app.get('/', function (req, res) {
     )
     res.status(301).send()
   }
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.send('root')
+  //res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.get('/*', function (req, res) {
   if (req.get('host').includes('app.agribalyse.fr')) {
     res.set(
       'location',
-      'https://www.agribalyse.fr/app' + req.originalUrl.replace('app/', '')
+      'https://www.agribalyse.fr/app' + req.originalUrl.replace('/app', '')
     )
     res.status(301).send()
   }
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.send('root')
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.listen(port)
