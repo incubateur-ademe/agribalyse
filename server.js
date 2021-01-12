336e5329b723fc778dd5cbb92e85b808c9ee0af5
florianpanchout <hi@florianpanchout.com>
2021-01-12 11:43:23 +0100

    Fix 301

--- server.js
+++ server.js
@@ -3,19 +3,8 @@ const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 app.use(express.static(__dirname))
-//app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/', function (req, res) {
-  if (req.get('host').includes('app.agribalyse.fr')) {
-    res.set(
-      'location',
-      'https://www.agribalyse.fr/app' + req.originalUrl.replace('app/', '')
-    )
-    res.status(301).send()
-  }
-  res.send('/')
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
 app.get('/*', function (req, res) {
   if (req.get('host').includes('app.agribalyse.fr')) {
     res.set(
