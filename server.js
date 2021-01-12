589456fc4eb7582bd1bb7b86725195130e1e7599
florianpanchout <hi@florianpanchout.com>
2021-01-12 10:51:15 +0100

    Redirection 301 home

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   if (req.get('host').includes('app.agribalyse.fr')) {
-    res.set('location', 'https://agribalyse.fr/app/' + req.originalUrl)
+    res.set('location', 'https://agribalyse.fr/app' + req.originalUrl)
     res.status(301).send()
   }
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
