d1f27d97a9033bb917b5b37dbc42ca894678a959
florianpanchout <hi@florianpanchout.com>
2021-01-12 10:55:12 +0100

    Test 301

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', function (req, res) {
-  if (req.get('host').includes('app.agribalyse.fr')) {
-    res.set('location', 'https://agribalyse.fr/app' + req.originalUrl)
-    res.status(301).send()
-  }
+  //if (req.get('host').includes('app.agribalyse.fr')) {
+  res.set('location', 'https://agribalyse.fr/app' + req.originalUrl)
+  res.status(301).send()
+  //}
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.listen(port)
