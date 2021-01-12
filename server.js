bff00db6a82355a73fe0b53a60f7b7a98d9d66ce
florianpanchout <hi@florianpanchout.com>
2021-01-12 11:09:43 +0100

    Redirect 301

--- server.js
+++ server.js
@@ -5,9 +5,22 @@ const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
+app.get('/', function (req, res) {
+  if (req.get('host').includes('app.agribalyse.fr')) {
+    res.set(
+      'location',
+      'https://www.agribalyse.fr/app' + req.originalUrl.replace('app/', '')
+    )
+    res.status(301).send()
+  }
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 app.get('/*', function (req, res) {
   if (req.get('host').includes('app.agribalyse.fr')) {
-    res.set('location', 'https://agribalyse.fr/app' + req.originalUrl)
+    res.set(
+      'location',
+      'https://www.agribalyse.fr/app' + req.originalUrl.replace('app/', '')
+    )
     res.status(301).send()
   }
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
